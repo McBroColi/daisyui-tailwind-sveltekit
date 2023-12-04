515bc123697c5f77816a53b58192bf3a3450bf71
McBroColi <mcbrocoli@gmail.com>
2023-12-04 09:41:58 -0700

    init

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+	  extend: {
+		  fontFamily: {
+			  'sans': ['Inter'],
+			  'code': ['FiraCode'],
+			  'serif': ['Cinzel'],
+			  'display': ['Baloo']
+		  }
+	  },
+	},
+	plugins: [require('daisyui')]
+}
